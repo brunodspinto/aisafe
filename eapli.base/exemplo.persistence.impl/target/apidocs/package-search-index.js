8956150a44a1c174cf25b0e911ae5536b61fed0d
Bruno Pinto <1250652@isep.ipp.pt>
2026-04-29 16:07:54 +0100

    US31 - add application layer, infrastructure, repositories and tests #21

--- eapli.base/exemplo.persistence.impl/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"eapli.exemplo.persistence.impl.inmemory"},{"l":"eapli.exemplo.persistence.impl.jpa"}];updateSearchResults();
